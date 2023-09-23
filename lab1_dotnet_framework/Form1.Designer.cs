c997aa5abf52739dc4a43b0d698f669957820b56
nik <lesnikowka@mail.ru>
2023-09-23 18:10:07 +0300

    gui

---
+++ lab1_dotnet_framework/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace lab1_dotnet_framework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выборТипаЗадачиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.тестоваяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.основнаяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(621, 69);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(625, 391);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Построить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(213, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Начальные условия:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X0:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "U0:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выборТипаЗадачиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1386, 33);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выборТипаЗадачиToolStripMenuItem
+            // 
+            this.выборТипаЗадачиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.тестоваяToolStripMenuItem,
+            this.основнаяToolStripMenuItem});
+            this.выборТипаЗадачиToolStripMenuItem.Name = "выборТипаЗадачиToolStripMenuItem";
+            this.выборТипаЗадачиToolStripMenuItem.Size = new System.Drawing.Size(185, 32);
+            this.выборТипаЗадачиToolStripMenuItem.Text = "Выбор типа задачи";
+            // 
+            // тестоваяToolStripMenuItem
+            // 
+            this.тестоваяToolStripMenuItem.Name = "тестоваяToolStripMenuItem";
+            this.тестоваяToolStripMenuItem.Size = new System.Drawing.Size(195, 34);
+            this.тестоваяToolStripMenuItem.Text = "Тестовая";
+            this.тестоваяToolStripMenuItem.Click += new System.EventHandler(this.тестоваяToolStripMenuItem_Click);
+            // 
+            // основнаяToolStripMenuItem
+            // 
+            this.основнаяToolStripMenuItem.Name = "основнаяToolStripMenuItem";
+            this.основнаяToolStripMenuItem.Size = new System.Drawing.Size(195, 34);
+            this.основнаяToolStripMenuItem.Text = "Основная";
+            this.основнаяToolStripMenuItem.Click += new System.EventHandler(this.основнаяToolStripMenuItem_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(446, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(446, 197);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(446, 295);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(446, 359);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1386, 689);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выборТипаЗадачиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem тестоваяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem основнаяToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ lab1_dotnet_framework/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab1_dotnet_framework
+{
+    enum TaskType
+    {
+        Test,
+        Main,
+        NotSelected
+    }
+
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.chart1.Series.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string x0Text = textBox1.Text;
+            string u0Text = textBox2.Text;
+            string startStepText = textBox3.Text;
+            string localPrecisionText = textBox4.Text;
+            string boundPrecisionText = textBox5.Text;
+            string maxStepNumbersText = textBox6.Text;
+
+            if (x0Text.Length == 0 || u0Text.Length == 0 || 
+                startStepText.Length == 0 || localPrecisionText.Length == 0 ||
+                boundPrecisionText.Length == 0 || maxStepNumbersText.Length == 0)
+            {
+                MessageBox.Show("Вы не ввели начальные условия", "Ошибка");
+                return;
+            }
+
+            x0Text.Replace('.', ',');
+            u0Text.Replace('.', ',');
+            startStepText.Replace(".", ",");
+            localPrecisionText.Replace(".", ",");   
+            boundPrecisionText.Replace('.', ',');
+
+
+            double X0;
+            double U0;
+            double startStep;
+            double localPrecision;
+            double boundPrecision;
+
+            int maxStepNumbers;
+
+            try
+            {
+                X0 = Convert.ToDouble(x0Text);
+                U0 = Convert.ToDouble(u0Text);
+                startStep = Convert.ToDouble(startStepText);
+                localPrecision = Convert.ToDouble(boundPrecisionText);
+                boundPrecision = Convert.ToDouble(maxStepNumbersText);  
+                maxStepNumbers = Convert.ToInt32(maxStepNumbersText);
+            }
+            catch 
+            {
+                MessageBox.Show("Неверный формат введенных значений", "Ошибка");
+                return;
+            }
+
+            Tuple<double, double> x0u0Tuple = new Tuple<double, double>(X0, U0);
+
+            List<Series> oldSeries = SeriesForStartConditions[x0u0Tuple];
+
+            for (int i = 0; i < oldSeries.Count; i++)
+            {
+                this.chart1.Series.Remove(oldSeries[i]);
+            }
+
+            SeriesForStartConditions.Remove(x0u0Tuple);
+
+            Series newSeries = new Series();
+
+            switch (SelectedTask)
+            {
+                case TaskType.Test:
+
+
+                    break;
+            }
+
+            newSeries.ChartType = SeriesChartType.Line;
+
+            newSeries.Points.Add(X0, U0);
+            newSeries.Points.Add(2*X0, 2*U0);
+
+            this.chart1.Series.Add(newSeries);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void тестоваяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            выборТипаЗадачиToolStripMenuItem.Text = "Тестовая";
+            SelectedTask = TaskType.Test;
+            this.chart1.Series.Clear();
+        }
+
+        private void основнаяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            выборТипаЗадачиToolStripMenuItem.Text = "Основная";
+            SelectedTask = TaskType.Main;
+            this.chart1.Series.Clear();
+        }
+
+        private double TrueSoluitonFunction(double X0, double U0)
+        {
+            return 3;
+        }
+
+        private TaskType SelectedTask = TaskType.NotSelected;
+
+        Dictionary<Tuple<double, double>, List<Series>> SeriesForStartConditions;
+
+    }
+}
